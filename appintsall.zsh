4d412ffebe285f5ea75a8a4d78db9c87e10410e3
fourteendp <fourteendp@qq.com>
2026-05-09 17:29:42 +0800

    update

--- appintsall.zsh
+++ appintsall.zsh
@@ -1,10 +1,8 @@
-sudo apt install zsh git curl wget eza zoxide fzf starship neovim axel rsync openssh-server unzip ranger
-
-# 安装
-apt install locales
-# 配置
-sudo dpkg-reconfigure locales
-
-apt install build-essential
-
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+# 下载工具
+sudo apt install curl wget git axel -y
+# 必要工具
+sudo apt install eza zoxide fzf rsync unzip ranger -y
+# 终端美化
+sudo apt install starship -y
+# 编辑器
+sudo apt install neovim -y
