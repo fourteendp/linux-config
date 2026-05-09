2d714eeb5883f33c8b0f1fbba1e8ffdadef44994
fourteendp <fourteendp@qq.com>
2026-05-09 17:43:54 +0800

    update

--- init.zsh
+++ init.zsh
@@ -3,6 +3,7 @@ export ZSHCONFIG="$HOME/.config/zsh"
 
 source $ZSHCONFIG/env.zsh          # 加载密钥
 source $ZSHCONFIG/system.zsh       # 环境变量
+source $ZSHCONFIG/apt.zsh          # 软件源
 
 # 自动加载当前目录下lib中的所有.zsh脚本
 for script in $ZSHCONFIG/lib/*.zsh; do
