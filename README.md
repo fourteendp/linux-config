d171d04b5e69bba36ffc56f2d02007f8649f71e4
fourteendp <fourteendp@qq.com>
2026-05-09 16:49:38 +0800

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+sudo apt install zsh git curl wget eza zoxide fzf starship neovim axel rsync openssh-server unzip ranger
+
+# 安装
+apt install locales
+# 配置
+sudo dpkg-reconfigure locales
+
+apt install build-essential
+
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
---
+++ sysinit.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+echo "========================================"
+echo "正在更新软件源 ..."
+apt update -y > /dev/null 2>&1
+echo "✅ 软件源更新完成"
+
+echo "========================================"
+echo "正在设置 root 密码为 root ..."
+passwd root << EOF
+root
+root
+EOF
+echo "✅ root 密码设置完成"
+
+
+echo "========================================"
+if ! id -u admin > /dev/null 2>&1; then
+    echo "正在创建 admin 用户 ..."
+    useradd -m -s /bin/bash admin
+    passwd admin << EOF
+admin
+admin
+EOF
+    usermod -aG sudo admin
+    echo "✅ admin 用户创建成功"
+    echo "🔑 用户名: admin  密码: admin"
+else
+    echo "✅ admin 用户已存在"
+fi
+
+echo "========================================"
+echo "正在安装 zsh ..."
+apt install -y zsh > /dev/null 2>&1
+touch /home/admin/.zshrc
+chown admin:admin /home/admin/.zshrc
+echo "✅ zsh 安装完成"
+
+echo "========================================"
+echo "正在设置 admin 默认 Shell 为 zsh ..."
+chsh -s $(which zsh) admin
+echo "✅ admin 默认 Shell 已切换为 zsh"
+
+echo "========================================"
+echo "🎉 所有初始化操作全部完成！"
+echo "========================================"
+echo "root  密码: root"
+echo "admin 密码: admin"
+echo "admin 默认 Shell: zsh"
+echo "========================================"
+echo ""
+echo "正在切换到 admin 用户 ..."
+echo ""
+
+su - admin
