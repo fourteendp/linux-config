38b486dd42d8d1d47e763dfe027cc4d9c4ccc045
fourteendp <fourteendp@qq.com>
2026-05-29 15:11:01 +0800

    update

--- init.zsh
+++ init.zsh
@@ -2,9 +2,9 @@
 export CONFIGDIR="$HOME/.config"
 export SOURCESDIR="$CONFIGDIR/sources"
 
+source $CONFIGDIR/ziminit.zsh           # zimfw
 source $CONFIGDIR/libs/env.zsh          # 加载密钥
 source $CONFIGDIR/libs/system.zsh       # 环境变量
 source $CONFIGDIR/libs/alias.zsh        # 别名
 source $CONFIGDIR/func/init.zsh         # 函数
-source $CONFIGDIR/ziminit.zsh           # zimfw
 source $CONFIGDIR/libs/plugins.zsh      # 插件
--- libs/alias.zsh
+++ libs/alias.zsh
@@ -4,15 +4,10 @@ alias zshr="source ~/.zshrc"
 
 
 if (($+commands[eza] )); then
-  # ls 命令
-  alias ls='eza -a --icons=auto'  # 自动判断是否是否显示图标（非纯文本环境）
-  alias l='eza -1a --icons'     # 单列显示，带图标
-
-  # 详细列表模式
-  alias ll='eza -l --icons --git --sort=type'  # 详细信息 + 图标 + Git 状态
-  alias la='eza -la --icons --git --sort=type' # 包含隐藏文件的详细列表
-  alias laa='eza -la --icons --git  --sort=type --total-size' # 包含隐藏文件的详细列表，按类型排序
-  alias l.='eza -ld .* --icons --git --sort=type'    # 只显示隐藏文件/目录
+  alias l='eza --icons=auto --git'
+  alias la='eza -a --icons=auto --git'
+  alias ll='eza -l --icons=auto --git --sort=type'
+  alias lla='eza -la --icons=auto --git --sort=type'
 fi
 
 if (($+commands[z] )); then
